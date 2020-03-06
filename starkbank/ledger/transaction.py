55628f7504d13d0100438df771730a24408f33b0
Caio Dottori <caio.dottori@starkbank.com>
2020-03-06 12:21:59 -0300

    fixed missing receiverId in incoming transactions

--- starkbank/ledger/transaction.py
+++ starkbank/ledger/transaction.py
@@ -1,6 +1,6 @@
 from starkbank import request
 from starkbank.utils.base import Base
-from starkbank.utils.case import snake_to_camel
+from starkbank.utils.case import snake_to_camel, camel_to_snake
 from starkbank.utils.checks import check_user, check_datetime
 
 
@@ -20,7 +20,7 @@ class Transaction(Base):
     }
     _known_camel_fields = {snake_to_camel(field) for field in _known_fields}
 
-    def __init__(self, amount, description, tags, external_id, receiver_id=None, sender_id=None, id=None, fee=None, created=None, source=None):
+    def __init__(self, amount, description, tags, external_id, receiver_id, sender_id=None, id=None, fee=None, created=None, source=None):
         Base.__init__(self, id=id)
 
         self.amount = amount
@@ -32,6 +32,13 @@ class Transaction(Base):
         self.fee = fee
         self.created = created
         self.source = source
+
+    @classmethod
+    def from_json(cls, json):
+        json.setdefault("receiverId", None)
+        return cls(**{
+            camel_to_snake(k): v for k, v in json.items() if k in cls._known_camel_fields
+        })
 
 
 def create(transactions, user=None):
