37b49e980194d3d1c4588b1e7386e4bcfd7a4581
Caio Dottori <caio.dottori@starkbank.com>
2020-03-13 22:51:53 -0300

    cleanup test keys

--- tests/utils/keys.py
+++ tests/utils/keys.py
@@ -2,7 +2,6 @@ from ellipticcurve.publicKey import PublicKey
 from ellipticcurve.privateKey import PrivateKey
 from ellipticcurve.utils.file import File
 
-privateKeyString = File.read("examples/keys/member/privateKey.pem")
-publicKeyString = File.read("examples/keys/member/publicKey.pem")
+
 privateKey = PrivateKey.fromPem(privateKeyString)
 publicKey = PublicKey.fromPem(publicKeyString)
