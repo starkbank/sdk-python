68c801892ba68e548377542fdab9485cee96fe31
Caio Dottori <caio.dottori@starkbank.com>
2020-03-16 15:26:30 -0300

    added keys.generate docstring

--- starkbank/keys.py
+++ starkbank/keys.py
@@ -3,6 +3,12 @@ from os import path as os_path
 
 
 def generate(path=None):
+    """Generates a secp256k1 ECDSA private/public key pair to be used in the API authentications
+
+    Parameters (optional):
+        path [string]: path to save the keys .pem files. No files will be saved if this parameter isn't provided
+    """
+
     private = PrivateKey()
     public = private.publicKey()
 
