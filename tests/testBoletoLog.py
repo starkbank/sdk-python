480a27c1eab0d9e6717ed9f58b06745bfe67b894
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-03-17 17:45:44 -0300

    Add boleto_ids test for BoletoLog

--- tests/testBoletoLog.py
+++ tests/testBoletoLog.py
@@ -1,7 +1,9 @@
+from time import sleep
 from unittest import TestCase, main
 
 import starkbank
 from starkbank.exception import InputErrors
+from tests.utils.boleto import generateExampleBoletosJson
 from tests.utils.user import exampleProject
 
 starkbank.user = exampleProject
@@ -12,6 +14,22 @@ class TestBoletoLogGet(TestCase):
     def test_success(self):
         logs = list(starkbank.boleto.log.query(limit=10))
         print("Number of logs:", len(logs))
+
+    def test_success_boleto_ids(self):
+        boletos = generateExampleBoletosJson(n=5)
+        boletos = starkbank.boleto.create(boletos)
+        boleto_ids = {boleto.id for boleto in boletos}
+        logs = starkbank.boleto.log.query(boleto_ids=boleto_ids)
+        log_result = {
+            "created": set(),
+            "registered": set(),
+        }
+        sleep(5)
+        for log in logs:
+            log_result[log.type].add(log.boleto.id)
+        print(log_result)
+        self.assertEqual(boleto_ids, log_result["created"])
+        self.assertEqual(boleto_ids, log_result["registered"])
 
 
 class TestBoletoLogInfoGet(TestCase):
