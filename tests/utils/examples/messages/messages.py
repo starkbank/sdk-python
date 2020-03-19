ee909562a76e0fcf69e01a436f90e3e43344a6b1
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-03-19 12:07:16 -0300

    Refactor example message path for robustness

--- tests/utils/examples/messages/messages.py
+++ tests/utils/examples/messages/messages.py
@@ -1,9 +1,12 @@
+import os
 from json import loads
 
 from ellipticcurve.utils.file import File
 
-exampleBoletosJson = loads(File.read("utils/examples/messages/exampleBoleto.json"))
-exampleBoletoPaymentsJson = loads(File.read("utils/examples/messages/exampleBoletoPayment.json"))
-exampleTransfersJson = loads(File.read("utils/examples/messages/exampleTransfer.json"))
-exampleTransactionsJson = loads(File.read("utils/examples/messages/exampleTransaction.json"))
-exampleUtilityPaymentsJson = loads(File.read("utils/examples/messages/exampleUtilityPayment.json"))
+dir = os.path.dirname(os.path.abspath(__file__))
+
+exampleBoletosJson = loads(File.read(os.path.join(dir, "exampleBoleto.json")))
+exampleBoletoPaymentsJson = loads(File.read(os.path.join(dir, "exampleBoletoPayment.json")))
+exampleTransfersJson = loads(File.read(os.path.join(dir, "exampleTransfer.json")))
+exampleTransactionsJson = loads(File.read(os.path.join(dir, "exampleTransaction.json")))
+exampleUtilityPaymentsJson = loads(File.read(os.path.join(dir, "exampleUtilityPayment.json")))
