781f7b547f65129d4b32af8cc07daa648f49bbb1
Caio Dottori <caio.dottori@starkbank.com>
2020-03-26 20:15:47 -0300

    changed tests/utils/user

--- tests/utils/user.py
+++ tests/utils/user.py
@@ -2,12 +2,12 @@ import starkbank
 
 exampleProject = starkbank.Project(
     environment="sandbox",
-    id="5690398416568320",
+    id="9999999999999999",
     private_key="""
     -----BEGIN EC PRIVATE KEY-----
-    MHQCAQEEIIoYWZ2OGwqX6n1EVvj1C1YvWHSGqqhZJzfsZZnk0SVgoAcGBSuBBAAK
-    oUQDQgAEGS1jWJXoK9RUk+qoNNFquO7X4JzRf5ZA5UDJUfPCbbKe5KwtrBKTJC1/
-    vRGIpAM5gNsxdfKgmoXNriiuY4LEPQ==
+    MHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK
+    oUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75
+    mSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==
     -----END EC PRIVATE KEY-----
     """,
 )
