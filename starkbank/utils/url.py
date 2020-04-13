4fd12fc4991138a58247bff2fc87517b05a00e01
Caio Dottori <caio.dottori@starkbank.com>
2020-04-13 15:15:00 -0300

    adopted casto to api json in urlencode

--- starkbank/utils/url.py
+++ starkbank/utils/url.py
@@ -1,7 +1,7 @@
-from .case import snake_to_camel
+from .api import cast_json_to_api_format
 
 
 def urlencode(params):
-    params = {k: v for k, v in params.items() if v is not None} if params else {}
-    params = {snake_to_camel(k): ",".join([str(value) for value in v]) if isinstance(v, (tuple, list, set)) else v for k, v in params.items()}
+    params = cast_json_to_api_format(params) if params else {}
+    params = {k: ",".join([str(value) for value in v]) if isinstance(v, (tuple, list, set)) else v for k, v in params.items()}
     return "?" + "&".join("{k}={v}".format(k=k, v=v) for k, v in params.items()) if params else ""
