a69f51a78f83b7333155cb514cc393653c023d09
Caio Dottori <caio.dottori@starkbank.com>
2020-04-16 11:46:10 -0300

    added after/before parameters to boleto payment log & utility payment log queries

--- starkbank/utilitypayment/log/__log.py
+++ starkbank/utilitypayment/log/__log.py
@@ -1,6 +1,6 @@
 from ...utils import rest
 from ...utils.api import from_api_json
-from ...utils.checks import check_datetime
+from ...utils.checks import check_datetime, check_date
 from ...utils.resource import Resource
 from ..__utilitypayment import _resource as _payment_resource
 
@@ -43,15 +43,17 @@ def get(id, user=None):
     return rest.get_id(resource=_resource, id=id, user=user)
 
 
-def query(limit=None, payment_ids=None, types=None, user=None):
+def query(limit=None, payment_ids=None, types=None, after=None, before=None, user=None):
     """# Retrieve utilitypayment.Log's
     Receive a generator of utilitypayment.Log objects previously created in the Stark Bank API
     ## Parameters (optional):
     - limit [integer, default None]: maximum number of objects to be retrieved. Unlimited if None. ex: 35
     - payment_ids [list of strings, default None]: list of UtilityPayment ids to filter retrieved objects. ex: ["5656565656565656", "4545454545454545"]
     - types [list of strings, default None]: filter retrieved objects by event types. ex: "paid" or "registered"
+    - after [datetime.date, default None] date filter for objects created only after specified date. ex: datetime.date(2020, 3, 10)
+    - before [datetime.date, default None] date filter for objects only before specified date. ex: datetime.date(2020, 3, 10)
     - user [Project object, default None]: Project object. Not necessary if starkbank.user was set before function call
     ## Return:
     - list of utilitypayment.Log objects with updated attributes
     """
-    return rest.get_list(resource=_resource, limit=limit, user=user, types=types, payment_ids=payment_ids)
+    return rest.get_list(resource=_resource, limit=limit, user=user, types=types, payment_ids=payment_ids, after=check_date(after), before=check_date(before))
