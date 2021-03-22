afb8532940997f94de6d56e852f9417e9fb0e1f9
Caio Dottori <caio.dottori@starkbank.com>
2021-03-22 11:55:31 -0300

    Add transaction_ids property to Boleto resource

--- starkbank/boleto/__boleto.py
+++ starkbank/boleto/__boleto.py
@@ -34,6 +34,7 @@ class Boleto(Resource):
     - line [string, default None]: generated Boleto line for payment. ex: "34191.09008 63571.277308 71444.640008 5 81960000000062"
     - bar_code [string, default None]: generated Boleto bar-code for payment. ex: "34195819600000000621090063571277307144464000"
     - status [string, default None]: current Boleto status. ex: "registered" or "paid"
+    - transaction_ids [list of strings]: ledger transaction ids linked to this boleto. ex: ["19827356981273"]
     - created [datetime.datetime, default None]: creation datetime for the Boleto. ex: datetime.datetime(2020, 3, 10, 10, 30, 0, 0)
     - our_number [string, default None]: Reference number registered at the settlement bank. ex:"10131474"
     """
@@ -41,7 +42,7 @@ class Boleto(Resource):
     def __init__(self, amount, name, tax_id, street_line_1, street_line_2, district, city, state_code, zip_code,
                  due=None, fine=None, interest=None, overdue_limit=None, tags=None, descriptions=None, discounts=None,
                  receiver_name=None, receiver_tax_id=None, id=None, fee=None, line=None, bar_code=None, status=None,
-                 created=None, our_number=None):
+                 transaction_ids=None, created=None, our_number=None):
         Resource.__init__(self, id=id)
 
         self.amount = amount
@@ -66,6 +67,7 @@ class Boleto(Resource):
         self.line = line
         self.bar_code = bar_code
         self.status = status
+        self.transaction_ids = transaction_ids
         self.created = check_datetime(created)
         self.our_number = our_number
 
