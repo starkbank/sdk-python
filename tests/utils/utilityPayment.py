10e765b771d5e87c1c4fc95da608e9a4f985de25
luistarkbank <luis.almeida@starkbank.com>
2025-02-12 15:01:18 -0300

    wipas

--- tests/utils/utilityPayment.py
+++ tests/utils/utilityPayment.py
@@ -5,6 +5,5 @@ def generateExampleUtilityPaymentsJson(n=1, amount=None, next_day=True):
     return generateExampleNonBoletoPaymentsJson(
         n=n,
         amount=amount,
-        next_day=next_day,
-        is_tax=True,
+        next_day=next_day
     )
