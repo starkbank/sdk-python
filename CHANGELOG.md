9caaff571b3fb9bbde9170112a58f6c1f7bf20f7
nicolasalmeida-stark <nicolas.almeida@starkbank.com>
2026-05-22 16:20:05 -0300

    Add PaymentLink resource

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ Given a version number MAJOR.MINOR.PATCH, increment:
 
 
 ## [Unreleased]
+### Added
+- PaymentLink resource
 
 ## [2.35.0] - 2026-06-29
 ### Added
---
+++ starkbank/paymentlink/__init__.py
@@ -0,0 +1,7 @@
+from .__paymentlink import create, get, query, page, update
+from .log.__log import Log
+from . import log
+from .attempt.__attempt import Attempt
+from . import attempt
+from .allowedinstallment.__allowedinstallment import AllowedInstallment
+from .item.__item import Item
